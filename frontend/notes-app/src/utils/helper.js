5dfa612739fd7c3296df6b7174f15ede1431dd6c
Shwetank jain <150837121+Shwetank00@users.noreply.github.com>
2024-07-04 02:51:31 +0530

    navbar profile card and searchbar component

--- frontend/notes-app/src/utils/helper.js
+++ frontend/notes-app/src/utils/helper.js
@@ -1,7 +1,17 @@
-
 export const validateEmail = (email) => {
-    // The regular expression to be used for validation.
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    // Use the test() method of the regular expression to check if the email address matches the pattern.
-    return regex.test(email);
+  // The regular expression to be used for validation.
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // Use the test() method of the regular expression to check if the email address matches the pattern.
+  return regex.test(email);
+};
+
+export const getInitials = (name) => {
+  if (!name) return "";
+  const words = name.split(" ");
+  let initials = "";
+
+  for (let i = 0; i < Math.min(words.length, 2); i++) {
+    initials += words[i][0];
+  }
+  return initials.toUpperCase();
 };
